9897b83a2cf858ac4daa1f3c3bd6f08a083c8470
websites <websites.unpinned074@passinbox.com>
2025-02-19 17:25:41 -0600

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# My scrap calculator for the new recycler update for Rust.
+
+This calculates the rate for scrapping items in Safe-zones and Monuments with their respective changes.
---
+++ scrappy.py
@@ -0,0 +1,13 @@
+import customtkinter
+from customtkinter import *
+
+#Window Settings
+root = CTk()
+root.title("Scrappy")
+root.iconbitmap('C:/Users/GameBoi/PycharmProjects/Scrappy 2.0/imgs/recycler.ico')
+root.geometry("1020x520")
+customtkinter.set_appearance_mode('dark')
+
+
+# Run Init
+root.mainloop()
