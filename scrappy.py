213043568c986bd45cccf7c16156e6500926fe30
websites <websites.unpinned074@passinbox.com>
2025-02-20 19:55:21 -0600

    Added Entry, Output, Clear function for the Frame, and Recycle Buttons.

--- scrappy.py
+++ scrappy.py
@@ -1,110 +1,147 @@
 import customtkinter
 from customtkinter import *
 
+
 #Window Settings
 root = CTk()
 root.title("Scrappy")
 root.iconbitmap('C:/Users/GameBoi/PycharmProjects/Scrappy 2.0/imgs/recycler.ico')
-root.geometry("1020x520")
+root.geometry("1000x500")
 customtkinter.set_appearance_mode('dark')
 
 
 # Frames
-frame = CTkFrame(master=root, height=500, width=1000, corner_radius=10)
-frame.grid(row=0, column=1, padx=10, pady=10)
+frame = CTkFrame(master=root, height=500, width=1000)
+frame.grid(row=0, column=1)
+
 
 # Button Group 1
 def comps():
-    trash = CTkButton(root, text="Tech Trash")
+    trash = CTkButton(frame, text="Tech Trash")
     trash.place(relx=0.3, rely=0.2, anchor=CENTER)
-    laptop = CTkButton(root, text="Computer")
+    laptop = CTkButton(frame, text="Computer")
     laptop.place(relx=0.45, rely=0.2, anchor=CENTER)
-    sheet = CTkButton(root, text="Sheet Metal")
+    sheet = CTkButton(frame, text="Sheet Metal")
     sheet.place(relx=0.6, rely=0.2, anchor=CENTER)
-    metal_blade = CTkButton(root, text="Blade")
+    metal_blade = CTkButton(frame, text="Blade")
     metal_blade.place(relx=0.3, rely=0.3, anchor=CENTER)
-    metal_pipe = CTkButton(root, text="Metal Pipe")
+    metal_pipe = CTkButton(frame, text="Metal Pipe")
     metal_pipe.place(relx=0.45, rely=0.3, anchor=CENTER)
-    gears = CTkButton(root, text="Gear")
+    gears = CTkButton(frame, text="Gear")
     gears.place(relx=0.6, rely=0.3, anchor=CENTER)
-    springs = CTkButton(root, text="Spring")
+    springs = CTkButton(frame, text="Spring")
     springs.place(relx=0.3, rely=0.4, anchor=CENTER)
-    smg = CTkButton(root, text="SMG Body")
+    smg = CTkButton(frame, text="SMG Body")
     smg.place(relx=0.45, rely=0.4, anchor=CENTER)
-    rfl = CTkButton(root, text="Rifle Body")
+    rfl = CTkButton(frame, text="Rifle Body")
     rfl.place(relx=0.6, rely=0.4, anchor=CENTER)
-    fse = CTkButton(root, text="Fuse")
+    fse = CTkButton(frame, text="Fuse")
     fse.place(relx=0.3, rely=0.5, anchor=CENTER)
-    trp = CTkButton(root, text="Tarp")
+    trp = CTkButton(frame, text="Tarp")
     trp.place(relx=0.45, rely=0.5, anchor=CENTER)
-    cam = CTkButton(root, text="Camera")
+    cam = CTkButton(frame, text="Camera")
     cam.place(relx=0.6, rely=0.5, anchor=CENTER)
-    rpe = CTkButton(root, text="Rope")
+    rpe = CTkButton(frame, text="Rope")
     rpe.place(relx=0.3, rely=0.6, anchor=CENTER)
-    semi = CTkButton(root, text="Semi Body")
+    semi = CTkButton(frame, text="Semi Body")
     semi.place(relx=0.45, rely=0.6, anchor=CENTER)
-    kit = CTkButton(root, text="Sewing Kit")
+    kit = CTkButton(frame, text="Sewing Kit")
     kit.place(relx=0.6, rely=0.6, anchor=CENTER)
-    tank = CTkButton(root, text="Propane Tank")
+    tank = CTkButton(frame, text="Propane Tank")
     tank.place(relx=0.45, rely=0.7, anchor=CENTER)
+
 
 #Button Group 2
 def tools():
-    pick = CTkButton(root, text="Ice Pick")
+    pick = CTkButton(frame, text="Ice Pick")
     pick.place(relx=0.3, rely=0.2, anchor=CENTER)
-    hammer = CTkButton(root, text="Salvaged Hammer")
+    hammer = CTkButton(frame, text="Salvaged Hammer")
     hammer.place(relx=0.45, rely=0.2, anchor=CENTER)
-    salvage = CTkButton(root, text="Salvaged Axe")
+    salvage = CTkButton(frame, text="Salvaged Axe")
     salvage.place(relx=0.6, rely=0.2, anchor=CENTER)
-    hatchet = CTkButton(root, text="Metal Hatchet")
+    hatchet = CTkButton(frame, text="Metal Hatchet")
     hatchet.place(relx=0.3, rely=0.3, anchor=CENTER)
-    pickaxe = CTkButton(root, text="Metal Pickaxe")
+    pickaxe = CTkButton(frame, text="Metal Pickaxe")
     pickaxe.place(relx=0.45, rely=0.3, anchor=CENTER)
-    prim_pick = CTkButton(root, text="Stone Pickaxe")
+    prim_pick = CTkButton(frame, text="Stone Pickaxe")
     prim_pick.place(relx=0.6, rely=0.3, anchor=CENTER)
-    prim_axe = CTkButton(root, text="Stone Axe")
+    prim_axe = CTkButton(frame, text="Stone Axe")
     prim_axe.place(relx=0.3, rely=0.4, anchor=CENTER)
-    jack = CTkButton(root, text="Jackhammer")
+    jack = CTkButton(frame, text="Jackhammer")
     jack.place(relx=0.45, rely=0.4, anchor=CENTER)
-    chain = CTkButton(root, text="Chainsaw")
+    chain = CTkButton(frame, text="Chainsaw")
     chain.place(relx=0.6, rely=0.4, anchor=CENTER)
-    shove = CTkButton(root, text="Shovel")
+    shove = CTkButton(frame, text="Shovel")
     shove.place(relx=0.3, rely=0.5, anchor=CENTER)
-    light = CTkButton(root, text="Flashlight")
+    light = CTkButton(frame, text="Flashlight")
     light.place(relx=0.45, rely=0.5, anchor=CENTER)
 
+
 #Button Group 3
-def melee():
-    swrd = CTkButton(root, text="Sword")
+def melees():
+    swrd = CTkButton(frame, text="Sword")
     swrd.place(relx=0.3, rely=0.2, anchor=CENTER)
-    long = CTkButton(root, text="Longsword")
+    long = CTkButton(frame, text="Longsword")
     long.place(relx=0.45, rely=0.2, anchor=CENTER)
-    salv = CTkButton(root, text="Salvaged Cleaver")
+    salv = CTkButton(frame, text="Salvaged Cleaver")
     salv.place(relx=0.6, rely=0.2, anchor=CENTER)
-    combat = CTkButton(root, text="Combat Knife")
+    combat = CTkButton(frame, text="Combat Knife")
     combat.place(relx=0.3, rely=0.3, anchor=CENTER)
-    bone = CTkButton(root, text="Bone Club")
+    bone = CTkButton(frame, text="Bone Club")
     bone.place(relx=0.45, rely=0.3, anchor=CENTER)
-    knife = CTkButton(root, text="Bone Knife")
+    knife = CTkButton(frame, text="Bone Knife")
     knife.place(relx=0.6, rely=0.3, anchor=CENTER)
-    pad = CTkButton(root, text="Paddle")
+    pad = CTkButton(frame, text="Paddle")
     pad.place(relx=0.3, rely=0.4, anchor=CENTER)
-    mce = CTkButton(root, text="Mace")
-    mce.place(relx=0.6, rely=0.4, anchor=CENTER)
-    skin = CTkButton(root, text="Skinning Knife")
-    skin.place(relx=0.3, rely=0.5, anchor=CENTER)
-    sspear = CTkButton(root, text="Stone Spear")
-    sspear.place(relx=0.45, rely=0.5, anchor=CENTER)
-    wspear = CTkButton(root, text="Wood Spear")
-    wspear.place(relx=0.6, rely=0.5, anchor=CENTER)
+    mce = CTkButton(frame, text="Mace")
+    mce.place(relx=0.45, rely=0.4, anchor=CENTER)
+    skin = CTkButton(frame, text="Skinning Knife")
+    skin.place(relx=0.6, rely=0.4, anchor=CENTER)
+    sspear = CTkButton(frame, text="Stone Spear")
+    sspear.place(relx=0.3, rely=0.5, anchor=CENTER)
+    wspear = CTkButton(frame, text="Wood Spear")
+    wspear.place(relx=0.45, rely=0.5, anchor=CENTER)
+
+
+# Clear Frame
+def comp():
+    for item in frame.winfo_children():
+        item.place_forget()
+    comps()
+def melee():
+    for item in frame.winfo_children():
+        item.place_forget()
+    melees()
+def tool():
+    for item in frame.winfo_children():
+        item.place_forget()
+    tools()
+
 
 # Tier Buttons
-Comps = CTkButton(frame, text="Comps", command=comps)
-Comps.place(relx=0.35, anchor=N)
-Melee = CTkButton(frame, text="Melee", command=melee)
-Melee.place(relx=0.50, anchor=N)
-Tools = CTkButton(frame, text="Tools", command=tools)
-Tools.place(relx=0.65, anchor=N)
+Comps = CTkButton(root, text="Comps", command=comp)
+Comps.place(relx=0.3, anchor=N)
+Melee = CTkButton(root, text="Melee", command=melee)
+Melee.place(relx=0.45, anchor=N)
+Tools = CTkButton(root, text="Tools", command=tools)
+Tools.place(relx=0.60, anchor=N)
+
+
+# Recycle Buttons
+monument = CTkButton(root, text="Monument")
+monument.place(relx=0.55, rely=0.82, anchor=S)
+safe = CTkButton(root, text="Safe")
+safe.place(relx=0.7, rely=0.82, anchor=S)
+
+
+# Entry
+entry = CTkEntry(root, width=200, height=5)
+entry.place(relx=0.35, rely=0.82, anchor=S)
+
+
+# Outputs
+output = CTkTextbox(master=root, width=520, height=50)
+output.place(relx=0.25, rely=0.85)
 
 # Exit
 def exiting():
