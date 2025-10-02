0456b06f76fd4df9aca9a370f055bb56044db00a
GameBoi <thegameboi@proton.me>
2025-10-01 20:21:03 -0500

    Created a button named "Click Me!" and added it to the new layout display.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Scrappy
 
 
-## My application for the game Rust, which calculates the amount of scrap you get from Safe-zone and Monument recyclers
-## in the newest May 2025 update.
+## My application for the game Rust, which calculates the amount of scrap you get from Safe-zone and Monument recyclers in the newest May 2025 update.
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PyQt6.QtWidgets import (QComboBox, QMessageBox, QPushButton, QLabel, QLineEdit, QGridLayout, QApplication,
-     QMainWindow, QGridLayout, QWidget, QTableWidget, QToolBar, QTextEdit, QStatusBar,
-     QTableWidgetItem, QDialog, QPlainTextEdit, QVBoxLayout)
+                             QMainWindow, QGridLayout, QWidget, QTableWidget, QToolBar, QTextEdit, QStatusBar,
+                             QTableWidgetItem, QDialog, QPlainTextEdit, QVBoxLayout, QSizePolicy, QHBoxLayout)
 from PyQt6.QtGui import QIcon, QAction
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
 import sys
 
 
@@ -11,7 +11,18 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Scrappy")
-        self.setGeometry(300, 300, 400, 400)
+        self.setGeometry(300, 300, 200, 100)
+
+        # Layout and Widgets
+        widget = QWidget(self)
+        layout = QVBoxLayout(widget)
+
+        button = QPushButton("Click Me!")
+
+
+        layout.addWidget(button)
+        self.setCentralWidget(widget)
+
 
 
 
@@ -20,4 +31,4 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
-app.exec()
+sys.exit(app.exec())
